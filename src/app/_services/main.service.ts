269176ddab3d611b46e9aaa30da5a8e2e006e235
SensibleHoneybee <sensible.honeybee@yahoo.com>
2022-10-01 10:34:00 +0100

    Add websocket support

--- src/app/_services/main.service.ts
+++ src/app/_services/main.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Observer, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Mode } from '../_models/mode';
 import { Instance } from '../_models/instance';
+import { WebsocketService } from './websocket.service';
+
+const WEBSERVICE_URL = "wss://xxxxxxxx.execute-api.eu-west-2.amazonaws.com/Prod";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +16,9 @@ export class MainService {
   private currentInstanceSubject: BehaviorSubject<Instance>;
   public currentInstance: Observable<Instance>;
   
-  constructor() {
+  public messages: Subject<any>;
+
+  constructor(private wsService: WebsocketService) {
     var storedModeString = sessionStorage.getItem('digitalMakerWebsiteMode')
     var storedMode = Mode.NotSelected;
     if (storedModeString != null) {
@@ -29,6 +35,12 @@ export class MainService {
     this.mode = this.modeSubject.asObservable();
     this.currentInstanceSubject = new BehaviorSubject<Instance>(null);
     this.currentInstance = this.currentInstanceSubject.asObservable();
+
+    this.messages = <Subject<any>>wsService.connect(WEBSERVICE_URL).pipe(map(
+      response => {
+        return response.data;
+      }
+    ));
   }
 
   public get currentInstanceValue(): Instance { return this.currentInstanceSubject.value; }
---
+++ src/app/_services/websocket.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable, Observer } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  private subject: Subject<MessageEvent>;
+
+  public connect(url: string): Subject<MessageEvent> {
+    if (!this.subject) {
+      this.subject = this.create(url);
+      console.log("Successfully connected: " + url);
+    }
+    return this.subject;
+  }
+
+  private create(url: string): Subject<MessageEvent> {
+    let ws = new WebSocket(url);
+
+    let observable = new Observable((obs: Observer<MessageEvent>) => {
+      ws.onmessage = obs.next.bind(obs);
+      ws.onerror = obs.error.bind(obs);
+      ws.onclose = obs.complete.bind(obs);
+      return ws.close.bind(ws);
+    });
+    let observer = {
+      next: (data: Object) => {
+        let sendMessage = { message: "sendmessage", data: JSON.stringify(data) };
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify(sendMessage));
+        } else {
+          // If websocket not yet open, keep trying once per second for 5 seconds
+          setTimeout(function () {
+            if (ws.readyState === WebSocket.OPEN) {
+              ws.send(JSON.stringify(sendMessage));
+            } else {
+              setTimeout(function () {
+                if (ws.readyState === WebSocket.OPEN) {
+                  ws.send(JSON.stringify(sendMessage));
+                } else {
+                  setTimeout(function () {
+                    if (ws.readyState === WebSocket.OPEN) {
+                      ws.send(JSON.stringify(sendMessage));
+                    } else {
+                      setTimeout(function () {
+                        if (ws.readyState === WebSocket.OPEN) {
+                          ws.send(JSON.stringify(sendMessage));
+                        } else {
+                          alert('Could not connect');
+                        }
+                      }, 1000);   
+                    }
+                  }, 1000);
+                }
+              }, 1000);    
+            }
+          }, 1000);
+        }
+      }
+    };
+    return Subject.create(observer, observable);
+  }
+}
